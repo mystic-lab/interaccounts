90beef4b20843d48146cf7d5dce557c3752cfe68
Joe Schnetzler <schnetzlerjoe@gmail.com>
2022-09-27 23:57:05 -0500

    Edit test structure

--- contract/test/test-contract.js
+++ contract/test/test-contract.js
@@ -12,13 +12,24 @@ import {
 } from '@agoric/swingset-vat/src/vats/network/index.js';
 import { Far } from '@endo/marshal';
 import { makePromiseKit } from '@endo/promise-kit';
+import bundleSource from '@endo/bundle-source';
+import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
+import { makeZoeKit } from '@agoric/zoe';
 
 const filename = new URL(import.meta.url).pathname;
 const dirname = path.dirname(filename);
 
 const contractPath = `${dirname}/../src/contract.js`;
 
-const testPublicFacet = async (t, publicFacet) => {
+const testPublicFacet = async (t) => {
+  const { zoeService } = makeZoeKit(makeFakeVatAdmin().admin);
+  const feePurse = E(zoeService).makeFeePurse();
+  const zoe = E(zoeService).bindDefaultFeePurse(feePurse);
+
+  const bundle = await bundleSource(contractPath);
+  const installation = await E(zoe).install(bundle);
+  const instance = await E(zoe).startInstance(installation);
+
   // Create a network protocol to be used for testing
   const protocol = makeNetworkProtocol(makeLoopbackProtocolHandler());
 
@@ -37,13 +48,13 @@ const testPublicFacet = async (t, publicFacet) => {
   const msgBytes = MsgSend.encode(msgType).finish();
 
   // Create a swap msg
-  const msg = await E(publicFacet).makeMsg({
+  const msg = await E(instance.publicFacet).makeMsg({
     typeUrl: '/cosmos.bank.v1beta1.MsgSend',
     value: msgBytes,
   });
 
   // Create an ICA packet with the swap msg
-  const sendPacket = await E(publicFacet).makeICAPacket([msg]);
+  const sendPacket = await E(instance.publicFacet).makeICAPacket([msg]);
 
   // Create first port that packet will be sent to
   const port = await protocol.bind('/loopback/foo');
@@ -75,7 +86,7 @@ const testPublicFacet = async (t, publicFacet) => {
       async onOpen(c, localAddr, remoteAddr, _connectionHandler) {
         t.is(localAddr, '/loopback/bar/nonce/1');
         t.is(remoteAddr, '/loopback/foo/nonce/2');
-        const pingack = await E(publicFacet).sendICAPacket(sendPacket, c);;
+        const pingack = await E(instance.publicFacet).sendICAPacket(sendPacket, c);;
         t.is(pingack, 'pingack', 'expected pingack');
         closed.resolve();
       },
@@ -88,7 +99,5 @@ const testPublicFacet = async (t, publicFacet) => {
 };
 
 test('raw - send interchain tx', async (t) => {
-  const mod = await import(contractPath);
-  const { publicFacet } = await mod.start();
-  await testPublicFacet(t, publicFacet);
+  await testPublicFacet(t);
 });
